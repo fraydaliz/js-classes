afc2fa38005fabbea77101bb03805b50ea99c26f
Fraydaliz Carpio <carpiofraydaliz19@gmail.com>
2025-03-24 22:30:22 -0400

    index.html and app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
+const alice = new Person("Alice", 30);
+
+alice.greet()
+
+
+// Class 
+
+class Person2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+
+    haveBirthday() {
+        this.age += 1;
+        console.log(`Happy Birthday, ${this.name}! You are now ${this.age} years old.`);
+    }
+
+}
+
+
+const Mary = new Person2("Mary", 30);
+
+
+Mary.greet(); 
+Mary.haveBirthday();
